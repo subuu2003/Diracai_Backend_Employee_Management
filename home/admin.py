da972efea17e2e53036fc0e4ee440961208b8bf3
subuu2003 <subratbarik2003@gmail.com>
2026-05-26 18:32:29 +0530

    blog and hero section

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
+from .models import SiteConfig, HeroVideo
 
-# Register your models here.
+
+@admin.register(SiteConfig)
+class SiteConfigAdmin(admin.ModelAdmin):
+    list_display = ("company_name", "hero_heading", "hero_highlight", "updated_at")
+    readonly_fields = ("updated_at",)
+
+    def has_add_permission(self, request):
+        return not SiteConfig.objects.exists()
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
+@admin.register(HeroVideo)
+class HeroVideoAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "duration", "is_selected", "order", "uploaded_at")
+    list_editable = ("title", "duration", "is_selected", "order")
+    list_filter = ("is_selected",)
+    ordering = ("order", "uploaded_at")
+    readonly_fields = ("uploaded_at",)
